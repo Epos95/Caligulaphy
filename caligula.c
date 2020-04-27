164aacab5c0030ab9c15b06e78efd1d566eb9eb3
epos95 <max.agnesund95@gmail.com>
2020-04-27 16:07:27 +0200

    moved test into caligula file did more stuff

--- caligula.c
+++ caligula.c
@@ -1,48 +1,116 @@
+#include <ncurses.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <curses.h>
+#include <time.h>
 
-void ctos(char* reference, char input);
+#define BACKGROUND  "\x1B[30m"
+#define ERROR 		"\x1B[31m"
+#define SUCCESS 	"\x1B[32m"
+#define RESET 		"\x1B[0m"
 
-int main(){
-	char test;
-	test = 'c';
-
-	initscr();
-	cbreak();
-
-	clear();
-
-	printw("lmao");
-	printw("haha");
-
-	/*
-	char* ref;
-	ctos(ref, 'c');
-	printw(ref);*/
-
-	refresh();
-	//getch();
-	
-	/*
-	while (1) {
-		char c = getch();
-
-		if (c == 'c')
-			break;
-		else
-			printw(c);
-	}*/
-
-	endwin();
+#define SUCCESS_PAIR 1
+#define ERROR_PAIR 2
+#define BACKGROUND_PAIR 3
 
 
-	return 0;
+/*
+ * använd röd färg för fel,
+ * grön färg för avklarade bokstäver,
+ * ingen färg för normal text.
+ */
+
+int count_words(char words[1024]) {
+	int amount = 1;
+
+	for (int i=0; i < strlen(words); i++){
+		if (words[i] == ' ') {
+			amount++;
+		}
+	}
+
+	return amount;
 }
 
-void ctos(char* reference,char input){
-	char output[2];
-	output[0] = input;
-	output[1] = '\0';
-	reference = output;
+
+int main()
+{
+	char str[1024] = "ah hello there it is so nice to meet you";
+	int words = count_words(str);
+
+	initscr();
+	noecho();
+
+	if (has_colors() == FALSE){
+		printf(ERROR "[Caligulaphy]\t\t" RESET);
+		printf("Color might not be supported.\n");
+		exit(1);
+	}
+
+	start_color();
+	init_pair(SUCCESS_PAIR, COLOR_GREEN, COLOR_BLACK);
+	init_pair(ERROR_PAIR, COLOR_RED, COLOR_BLACK);
+	init_pair(BACKGROUND_PAIR, COLOR_BLACK, COLOR_BLACK);
+
+	attron(COLOR_PAIR(BACKGROUND_PAIR));
+	for(int i = 0; i < LINES; i++){
+		mvhline(i,0,' ', COLS);
+	}
+	attroff(COLOR_PAIR(BACKGROUND_PAIR));
+
+
+
+	mvprintw(10,10,str);
+
+	char input;
+	char current;
+	int start_of_word = 0;
+	int length_of_word = 0;
+	int errors = 0;
+	int total_errors = 0;
+	clock_t start, end;
+
+
+	start = clock();
+
+	for (int i=0; i < strlen(str); i++){
+		move(10,10+i);
+
+		current = str[i];
+		input = getch();
+		
+		if (current == ' ' || i == strlen(str)) {
+			// ordet är slut
+			if (errors > 0){
+				errors = 0;
+				total_errors++;
+			}
+
+
+			start_of_word = i+1;
+			// sätt nytt start för ett nytt ord
+			length_of_word = 0;
+			// resetta längden av ordet
+		}
+
+		if(input == current){
+			attron(COLOR_PAIR(SUCCESS_PAIR));
+			mvaddch(10,10+i, current);
+			attroff(COLOR_PAIR(SUCCESS_PAIR));
+		} else {
+			attron(COLOR_PAIR(ERROR_PAIR));
+			mvaddch(10,10+i, current);
+			attroff(COLOR_PAIR(ERROR_PAIR));
+			errors++;
+		}
+		refresh();
+	}
+	end = clock();
+
+	mvprintw(13,10, "You did well!\tpress any key to exit.\n%d errors %d", total_errors);
+	getch();
+	getch();
+	endwin();
+
+	return 0;
 }
--- makefile
+++ makefile
@@ -1,14 +1,10 @@
-CFLAGS = -Wall -g 
+CFLAGS = -Wall -g
 f = exe
 all:
 	gcc -lncurses caligula.c -o $f
-	gcc -lncurses test.c -o test
 
 run:
 	./$f
 
-
 clean:
 	rm -f $f
-	rm -f test
-
